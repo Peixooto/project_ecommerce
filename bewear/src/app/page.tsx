873744db4505708f7f3d350b42cea88c10555b79
Lucas Peixoto <lucas.peixoto@indt.org.br>
2025-08-13 16:03:09 -0400

    feat: auth with google

--- bewear/src/app/page.tsx
+++ bewear/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/ui/common/category-selector";
+import Footer from "@/components/ui/common/footer";
 import { Header } from "@/components/ui/common/header";
 import ProductList from "@/components/ui/common/products-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () =>{
   const products = await db.query.productTable.findMany({
@@ -12,6 +15,12 @@ const Home = async () =>{
     },
   });
 
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants:true,
+    },
+  });
   const categories = await db.query.categoryTable.findMany({});
   
   return (
@@ -33,8 +42,9 @@ const Home = async () =>{
         <div className="px-5">
          <Image src="/banner2.png" alt="texto" width={0} height={0} sizes="100vw" className="w-full h-auto"/>
         </div>
-
+        <ProductList products={newlyCreatedProducts} title="Novidades"/>
       </div>
+      <Footer/>
 
     </>
   );
