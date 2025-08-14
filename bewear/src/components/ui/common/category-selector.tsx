589fe5d96f2463ea002d95007eb8fd5bc27e5e7c
Lucas Peixoto <lucas.peixoto@indt.org.br>
2025-08-14 14:50:26 -0400

    feat: add category products page

--- bewear/src/components/ui/common/category-selector.tsx
+++ bewear/src/components/ui/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { categoryTable } from "@/db/schema";
 
 import { Button } from "../button";
@@ -8,13 +10,14 @@ interface CategorySelectorProps{
 
 const CategorySelector =({categories}: CategorySelectorProps)=>{
     return (
-    <div className="rounded-3xl bg-[#F4EFFF]">
-      <div className="grid grid-cols-2 gap-3">
+    <div className="rounded-3xl bg-[#F4EFFF] p-6">
+      <div className="grid grid-cols-2 gap-3 ">
         {categories.map((category) => (
         <Button key={category.id} 
         variant="ghost" 
         className="bg-white rounded-full">
-         {category.name}
+         
+         <Link href={`/category/${category.slug}`}>{category.name}</Link>
          </Button>
         ))}
       </div>
